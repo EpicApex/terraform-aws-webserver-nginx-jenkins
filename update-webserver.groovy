ec90124d905baa27e0f86869769a2ea1c5d239db
Omri Freidenberg <omri.f@uveye.com>
2022-10-17 02:24:05 +0300

    syntax

--- update-webserver.groovy
+++ update-webserver.groovy
@@ -17,7 +17,7 @@ node('memyselfandi'){
                 dir('/exercise/terraform-aws-webserver-nginx-jenkins'){
                     sh """
                         kubectl delete -n develop statefulset/nginx
-                        kubectl apply -n develop exercise/terraform-aws-webserver-nginx-jenkins/webserver-deployment/nginx-statefulset.yml
+                        kubectl apply -n develop -f /exercise/terraform-aws-webserver-nginx-jenkins/webserver-deployment/nginx-statefulset.yml
                         kubectl rollout status -n develop statefulset/nginx -v2
                         kubectl cp /exercise/terraform-aws-webserver-nginx-jenkins/webserver-deployment/index.html develop/nginx-0:/root
                         kubectl exec -n develop pod/nginx-0 -- chmod 611 /root
